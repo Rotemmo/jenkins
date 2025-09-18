73ea0c9b6d99f1fe357968fce0393d28c8093e40
Rotem Mo <rotemmo@post.bgu.ac.il>
2025-09-18 19:36:21 +0300

    update jenkinsfile

--- scripts/hello.sh
+++ scripts/hello.sh
@@ -1,7 +1,15 @@
 #!/usr/bin/env bash
+#handle error:
 set -euo pipefail
 
-NAME=$1
-echo "Date: $(date)"
-echo "Commit: $(git rev-parse HEAD || echo 'N/A')"
+#NAME parameter (we put in jenkins file 'world' as a default value)
+NAME=${1:-world}
+
+# Print current date
+echo "Current date: $(date)"
+
+# Print Git commit SHA, or N/A if not available
+echo "Git commit SHA: $(git rev-parse HEAD || echo 'N/A')"
+
+# Print Git commit SHA, or N/A if not available
 echo "Hello, $NAME"
